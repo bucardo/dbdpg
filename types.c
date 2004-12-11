150e086c6ad403bd76a56b0d060b2ddc61f282fa
Greg Sabino Mullane <greg@turnstep.com>
2004-12-11 04:01:48 +0000

    Minor cleanup of types: add in all _TYPE_ versions

--- types.c
+++ types.c
@@ -65,7 +65,7 @@ static sql_type_info_t pg_types[] = {
 	{BPCHAROID, "bpchar", DBDPG_TRUE, quote_char, dequote_char, {SQL_CHAR}},
 	{VARCHAROID, "varchar", DBDPG_TRUE, quote_varchar, dequote_varchar, {SQL_VARCHAR}},
 	{DATEOID, "date", DBDPG_TRUE, null_quote, null_dequote, {SQL_DATE}},
-	{TIMEOID, "time", DBDPG_TRUE, null_quote, null_dequote, {SQL_TIME}},
+	{TIMEOID, "time", DBDPG_TRUE, null_quote, null_dequote, {SQL_TYPE_TIME}},
 	{TIMESTAMPOID, "timestamp", DBDPG_TRUE, null_quote, null_dequote, {SQL_TYPE_TIMESTAMP}},
 	{TIMESTAMPTZOID, "datetime", DBDPG_TRUE, null_quote, null_dequote, {SQL_TYPE_TIMESTAMP_WITH_TIMEZONE}},
 	{INTERVALOID, "timespan", DBDPG_TRUE, null_quote, null_dequote, {0}},
@@ -178,8 +178,8 @@ static sql_type_info_t sql_types[] = {
 	{SQL_REAL, "SQL_REAL", DBDPG_TRUE, null_quote, null_dequote, {FLOAT8OID}},
 	{SQL_DOUBLE, "SQL_DOUBLE", DBDPG_TRUE, null_quote, null_dequote, {INT8OID}},
 	{SQL_BINARY, "SQL_BINARY", DBDPG_TRUE, quote_sql_binary, dequote_sql_binary, {BYTEAOID}},
-	{SQL_DATE, "SQL_DATE", DBDPG_TRUE, quote_varchar, dequote_varchar, {DATEOID}},
-	{SQL_TIME, "SQL_TIME", DBDPG_TRUE, quote_varchar, dequote_varchar, {TIMEOID}},
+	{SQL_TYPE_DATE, "SQL_TYPE_DATE", DBDPG_TRUE, quote_varchar, dequote_varchar, {DATEOID}},
+	{SQL_TYPE_TIME, "SQL_TYPE_TIME", DBDPG_TRUE, quote_varchar, dequote_varchar, {TIMEOID}},
 	{SQL_TYPE_TIMESTAMP, "SQL_TYPE_TIMESTAMP", DBDPG_TRUE, quote_varchar, dequote_varchar, {TIMESTAMPOID}},
 	{SQL_TYPE_TIMESTAMP_WITH_TIMEZONE, "SQL_TYPE_TIMESTAMP_WITH_TIMEZONE", DBDPG_TRUE, quote_varchar, dequote_varchar, {TIMESTAMPTZOID}},
 	{SQL_BOOLEAN, "SQL_BOOLEAN", DBDPG_TRUE,quote_bool, dequote_bool, {BOOLOID}},
@@ -200,8 +200,11 @@ sql_type_data(sql_type)
 	case SQL_REAL: return &sql_types[7];
 	case SQL_DOUBLE: return &sql_types[8];
 	case SQL_BINARY: return &sql_types[9];
-	case SQL_DATE: return &sql_types[10];
-	case SQL_TIME: return &sql_types[11];
+	case SQL_DATE:
+	case SQL_TYPE_DATE: return &sql_types[10];
+	case SQL_TIME:
+	case SQL_TYPE_TIME: return &sql_types[11];
+	case SQL_TIMESTAMP:
 	case SQL_TYPE_TIMESTAMP: return &sql_types[12];
 	case SQL_TYPE_TIMESTAMP_WITH_TIMEZONE: return &sql_types[13];
 	case SQL_BOOLEAN: return &sql_types[14];
