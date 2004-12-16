425a05e0124ddd383813abe35187033a8d4e124e
Greg Sabino Mullane <greg@turnstep.com>
2004-12-16 16:51:34 +0000

    Cleanup and improve comments.

--- dbdimp.h
+++ dbdimp.h
@@ -2,8 +2,8 @@
 	$Id$
 	
 	Copyright (c) 2000-2004 PostgreSQL Global Development Group
-	Copyright (c) 1997,1998,1999,2000 Edmund Mergl
-	Portions Copyright (c) 1994,1995,1996,1997 Tim Bunce
+	Portions Copyright (c) 1997-2000 Edmund Mergl
+	Portions Copyright (c) 1994-1997 Tim Bunce
 	
 	You may distribute under the terms of either the GNU General Public
 	License or the Artistic License, as specified in the Perl README file.
@@ -14,7 +14,6 @@
 #ifdef WIN32
 #define snprintf _snprintf
 #endif
-/* XXX - is snprintf used anywhere? -gsm */
 
 #define sword  signed int
 #define sb2    signed short
@@ -29,16 +28,17 @@ struct imp_drh_st {
 struct imp_dbh_st {
 	dbih_dbc_t com;          /* MUST be first element in structure */
 
+	bool    pg_bool_tf;      /* do bools return 't'/'f'? Set by user, default is 0 */
+	bool    pg_enable_utf8;  /* should we attempt to make utf8 strings? Set by user, default is 0 */
+	bool    prepare_now;     /* force immediate prepares, even with placeholders. Set by user, default is 0 */
+	char    pg_errorlevel;   /* PQsetErrorVerbosity. Set by user, defaults to 1 */
+	char    server_prepare;  /* do we want to use PQexecPrepared? 0=no 1=yes 2=smart. Can be changed by user */
+
 	PGconn  *conn;           /* connection structure */
 	bool    done_begin;      /* have we done a begin? (e.g. are we in a transaction?) */
-	bool    pg_bool_tf;      /* do bools return 't'/'f'? */
-	bool    pg_enable_utf8;  /* should we attempt to make utf8 strings? */
-	int     pg_protocol;     /* value of PQprotocolVersion usually 0, 2, or 3 */
-	char    server_prepare;  /* do we want to use PQexecPrepared? 0=no 1=yes 2=smart */
+	int     pg_protocol;     /* value of PQprotocolVersion, usually 0, 2, or 3 */
 	int     prepare_number;  /* internal prepared statement name modifier */
-	bool    prepare_now;     /* force immediate prepares, even with placeholders */
   char    *sqlstate;       /* from the last result */
-	char    errorlevel;      /* PQsetErrorVerbosity, defaults to 1 */
 };
 
 
