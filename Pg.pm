3e37398d3db8b7eb83e6ed75573d80c655da536f
Greg Sabino Mullane <greg@turnstep.com>
2006-02-26 19:04:23 +0000

    Jumping to 1.45

--- Pg.pm
+++ Pg.pm
@@ -16,7 +16,7 @@ use 5.006001;
 {
 	package DBD::Pg;
 
-	our $VERSION = '1.44_1';
+	our $VERSION = '1.45';
 	
 	use DBI ();
 	use DynaLoader ();
@@ -1447,7 +1447,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 1.44_1 of the DBD::Pg module
+This documents version 1.45 of the DBD::Pg module
 
 =head1 SYNOPSIS
 
