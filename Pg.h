e25413ba129486411fb607835c984e88e3763559
Greg Sabino Mullane <greg@turnstep.com>
2008-02-17 01:19:39 +0000

    Cleanups, add PERL_NO_GET_CONTEXT

--- Pg.h
+++ Pg.h
@@ -23,35 +23,28 @@ static int errno;
 
 #include "libpq-fe.h"
 
-#ifdef NEVER
-#include<sys/stat.h>
-#include "libpq/libpq-fs.h"
-#endif
-#ifndef INV_READ
-#define INV_READ 0x00040000
-#endif
-#ifndef INV_WRITE
-#define INV_WRITE 0x00020000
-#endif
-
 #ifdef BUFSIZ
 #undef BUFSIZ
 #endif
 /* this should improve I/O performance for large objects */
 #define BUFSIZ 32768
 
-
 #define NEED_DBIXS_VERSION 93
 
-#include <DBIXS.h>		/* installed by the DBI module	*/
+#define PERL_NO_GET_CONTEXT
 
-#include <dbd_xsh.h>		/* installed by the DBI module	*/
+#include <DBIXS.h>      /* installed by the DBI module */
+
+#include <dbivport.h>   /* DBI portability macros */
+
+#include <dbd_xsh.h>    /* installed by the DBI module */
 
 #include "dbdimp.h"
 #include "quote.h"
 #include "types.h"
 
 /* defines for Driver.xst to let it know what functions to include */
+
 #define dbd_st_rows dbd_st_rows
 #define dbd_discon_all dbd_discon_all
 #define dbd_st_fetchrow_hashref valid
