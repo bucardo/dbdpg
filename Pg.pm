38ee0c009a056eb8bb8336f7279f2227c9220c6d
Greg Sabino Mullane <greg@turnstep.com>
2008-02-18 23:36:58 +0000

    Bump to RC 3, mostly to check the 99cleanup.t problem.

--- Pg.pm
+++ Pg.pm
@@ -17,7 +17,7 @@ use 5.006001;
 {
 	package DBD::Pg;
 
-	use version; our $VERSION = qv("2.0.1_2");
+	use version; our $VERSION = qv("2.0.1_3");
 
 	use DBI ();
 	use DynaLoader ();
@@ -1625,7 +1625,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 2.0.1_2 of the DBD::Pg module
+This documents version 2.0.1_3 of the DBD::Pg module
 
 =head1 SYNOPSIS
 
