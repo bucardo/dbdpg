50327280712efd6dfd95bf755df2e7d18721dedc
Greg Sabino Mullane <greg@turnstep.com>
2008-04-14 15:39:10 +0000

    Change version number for a release candidate.

--- Pg.pm
+++ Pg.pm
@@ -17,7 +17,7 @@ use 5.006001;
 {
 	package DBD::Pg;
 
-	use version; our $VERSION = qv('2.6.0');
+	use version; our $VERSION = qv('2.5.2_1');
 
 	use DBI ();
 	use DynaLoader ();
@@ -1657,7 +1657,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 2.6.0 of the DBD::Pg module
+This documents version 2.5.2_1 of the DBD::Pg module
 
 =head1 SYNOPSIS
 
