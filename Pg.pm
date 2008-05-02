99a363e236524ca80213227f115d0dcec1d5c9e1
Greg Sabino Mullane <greg@turnstep.com>
2008-05-02 16:51:05 +0000

    Bump version to 2.6.4, minor test tweaks.

--- Pg.pm
+++ Pg.pm
@@ -17,7 +17,7 @@ use 5.006001;
 {
 	package DBD::Pg;
 
-	use version; our $VERSION = qv('2.6.3');
+	use version; our $VERSION = qv('2.6.4');
 
 	use DBI ();
 	use DynaLoader ();
@@ -1657,7 +1657,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 2.6.3 of the DBD::Pg module
+This documents version 2.6.4 of the DBD::Pg module
 
 =head1 SYNOPSIS
 
