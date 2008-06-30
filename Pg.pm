fd4e8ba863996147fd37aa49be40ad72077e4008
Greg Sabino Mullane <greg@turnstep.com>
2008-06-30 00:36:43 +0000

    Version bump.

--- Pg.pm
+++ Pg.pm
@@ -1688,7 +1688,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 2.8.1 of the DBD::Pg module
+This documents version 2.8.2 of the DBD::Pg module
 
 =head1 DESCRIPTION
 
