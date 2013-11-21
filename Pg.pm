cbb3a738a0d37fdff23471e95e57d776f5984ddf
Greg Sabino Mullane <greg@endpoint.com>
2013-11-20 22:19:15 -0500

    Version 2.20.1_2

--- Pg.pm
+++ Pg.pm
@@ -16,7 +16,7 @@ use 5.008001;
 {
 	package DBD::Pg;
 
-	use version; our $VERSION = qv('2.20.1_1');
+	use version; our $VERSION = qv('2.20.1_2');
 
 	use DBI ();
 	use DynaLoader ();
@@ -1773,7 +1773,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 2.20.1_1 of the DBD::Pg module
+This documents version 2.20.1_2 of the DBD::Pg module
 
 =head1 DESCRIPTION
 
