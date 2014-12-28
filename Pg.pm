54f735d0c1bdfdb4dafbf8c906130401f8430a94
Greg Sabino Mullane <greg@endpoint.com>
2014-12-27 21:26:09 -0500

    Bump version to 3.5.0: recent ping() changes require this.

--- Pg.pm
+++ Pg.pm
@@ -16,7 +16,7 @@ use 5.008001;
 {
 	package DBD::Pg;
 
-	use version; our $VERSION = qv('3.4.2');
+	use version; our $VERSION = qv('3.5.0');
 
 	use DBI ();
 	use DynaLoader ();
@@ -1682,7 +1682,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 3.4.2 of the DBD::Pg module
+This documents version 3.5.0 of the DBD::Pg module
 
 =head1 DESCRIPTION
 
