bucardo/dbdpg

commit 8ca0f67807848143470178fb535ea53cdcd8aad2

Always clear out libpq results before reassigning: fixes leak in putcopyend

dbdimp.c
@@ -4183,6 +4183,7 @@ int pg_db_getcopydata (SV * dbh, SV * dataline, int async)
 		result = PQgetResult(imp_dbh->conn);
 		status = _sqlstate(aTHX_ imp_dbh, result);
 		while (result != NULL) {
+                  	PQclear(result);
 			result = PQgetResult(imp_dbh->conn);
 		}
 		TRACE_PQCLEAR;
@@ -4281,6 +4282,7 @@ int pg_db_putcopyend (SV * dbh)
 		result = PQgetResult(imp_dbh->conn);
 		status = _sqlstate(aTHX_ imp_dbh, result);
 		while (result != NULL) {
+                  	PQclear(result);
 			result = PQgetResult(imp_dbh->conn);
 		}
 		TRACE_PQCLEAR;

commit 33cc2ad74161aea9eb040785cb55a95e5e23b6a8

Fix inconsistent indentation introduced by commit 8ca0f678

dbdimp.c
@@ -4183,7 +4183,7 @@ int pg_db_getcopydata (SV * dbh, SV * dataline, int async)
 		result = PQgetResult(imp_dbh->conn);
 		status = _sqlstate(aTHX_ imp_dbh, result);
 		while (result != NULL) {
-                  	PQclear(result);
+			PQclear(result);
 			result = PQgetResult(imp_dbh->conn);
 		}
 		TRACE_PQCLEAR;
@@ -4282,7 +4282,7 @@ int pg_db_putcopyend (SV * dbh)
 		result = PQgetResult(imp_dbh->conn);
 		status = _sqlstate(aTHX_ imp_dbh, result);
 		while (result != NULL) {
-                  	PQclear(result);
+			PQclear(result);
 			result = PQgetResult(imp_dbh->conn);
 		}
 		TRACE_PQCLEAR;