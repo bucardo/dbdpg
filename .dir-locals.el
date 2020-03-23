7707cc450e3beaaa5134474ee695c811b5e9b2d4
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2020-03-23 19:48:06 +0000

    Add .dir-locals.el to keep code more consistent

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((indent-tabs-mode . nil)))	; all modes
+ (cperl-mode . ((cperl-indent-level . 4)
+                (cperl-merge-trailing-else . nil)))
+ (c-mode . ((c-indentation-style . bsd)
+            (c-basic-offset . 4))))
