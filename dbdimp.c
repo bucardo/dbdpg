f3e1b9e974f247dafccdef35ff90c49c233e84d9
Greg Sabino Mullane <greg@turnstep.com>
2020-04-17 15:01:27 -0400

    Set version to 3.11.0

--- dbdimp.c
+++ dbdimp.c
@@ -494,7 +494,7 @@ int dbd_db_ping (SV * dbh)
     }
 
     /* No matter what state we are in, send an empty query to the backend */
-    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.10.5 */");
+    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.11.0 */");
     status = PQresultStatus(result);
     PQclear(result);
     if (PGRES_FATAL_ERROR == status) {
