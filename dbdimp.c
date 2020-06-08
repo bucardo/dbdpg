457b0f1ef3da455a563491f819a4d5b415e843ae
Greg Sabino Mullane <greg@turnstep.com>
2020-06-08 16:36:08 -0400

    Beta version 3.12.3_1

--- dbdimp.c
+++ dbdimp.c
@@ -494,7 +494,7 @@ int dbd_db_ping (SV * dbh)
     }
 
     /* No matter what state we are in, send an empty query to the backend */
-    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.12.4 */");
+    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.12.3_1 */");
     status = PQresultStatus(result);
     PQclear(result);
     if (PGRES_FATAL_ERROR == status) {
--- lib/Bundle/DBD/Pg.pm
+++ lib/Bundle/DBD/Pg.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use 5.008001;
 
-our $VERSION = '3.12.4';
+our $VERSION = '3.12.3_1';
 
 1;
 
