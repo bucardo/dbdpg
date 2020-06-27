0e0ecc65633d7683bd940327c95bf24547213d54
Greg Sabino Mullane <greg@turnstep.com>
2020-06-27 12:07:35 -0400

    Bump to version 3.13.1

--- dbdimp.c
+++ dbdimp.c
@@ -499,7 +499,7 @@ int dbd_db_ping (SV * dbh)
     }
 
     /* No matter what state we are in, send an empty query to the backend */
-    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.13.0 */");
+    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.13.1 */");
     status = PQresultStatus(result);
     PQclear(result);
     if (PGRES_FATAL_ERROR == status) {
--- lib/Bundle/DBD/Pg.pm
+++ lib/Bundle/DBD/Pg.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use 5.008001;
 
-our $VERSION = '3.13.0';
+our $VERSION = '3.13.1';
 
 1;
 
