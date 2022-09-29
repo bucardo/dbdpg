ba2327eec62cfc0245ffa6853e0fa21e22ebd318
unknown <C:\Users\\AppData\Roaming\The Bat!>
2022-09-29 15:13:42 +0300

    Fixed a hangup when processing the COPY command in asynchronous mode. Fix problem https://github.com/bucardo/dbdpg/issues/98

--- dbdimp.c
+++ dbdimp.c
@@ -5318,6 +5318,9 @@ long pg_db_result (SV *h, imp_dbh_t *imp_dbh)
             TRACE_PQCLEAR;
             PQclear(result);
         }
+        if ( rows == -1 ) {
+            break;
+        }
     }
 
     if (NULL != imp_dbh->async_sth) {
