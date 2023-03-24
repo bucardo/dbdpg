871174476602f05e6a577fd186b4162e65acd4fd
Greg Sabino Mullane <greg@turnstep.com>
2023-03-24 15:58:53 -0400

    Bump version to 3.16.2

--- Pg.pm
+++ Pg.pm
@@ -16,7 +16,7 @@ use 5.008001;
 {
     package DBD::Pg;
 
-    use version; our $VERSION = qv('3.16.1');
+    use version; our $VERSION = qv('3.16.2');
 
     use DBI 1.614 ();
     use Exporter ();
@@ -137,7 +137,7 @@ use 5.008001;
         $class .= '::dr';
 
         ## Work around for issue found in https://rt.cpan.org/Ticket/Display.html?id=83057
-        my $realversion = qv('3.16.1');
+        my $realversion = qv('3.16.2');
 
         $drh = DBI::_new_drh($class, {
             'Name'        => 'Pg',
@@ -1699,7 +1699,7 @@ DBD::Pg - PostgreSQL database driver for the DBI module
 
 =head1 VERSION
 
-This documents version 3.16.1 of the DBD::Pg module
+This documents version 3.16.2 of the DBD::Pg module
 
 =head1 DESCRIPTION
 
@@ -2796,7 +2796,7 @@ server version 9.0 or higher.
 
 The C<ping> method determines if there is a working connection to an active 
 database server. It does this by sending a small query to the server, currently 
-B<'DBD::Pg ping test v3.16.1'>. It returns 0 (false) if the connection is not valid, 
+B<'DBD::Pg ping test v3.16.2'>. It returns 0 (false) if the connection is not valid, 
 otherwise it returns a positive number (true). The value returned indicates the 
 current state:
 
