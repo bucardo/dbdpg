85d2ae0abae0dcee2905cd4905f4f6e54479e4cc
Greg Sabino Mullane <greg@turnstep.com>
2023-12-05 15:43:30 -0500

    Bump version to 3.18.0
    Put subscribe instructions for various mailing lists

--- dbdimp.c
+++ dbdimp.c
@@ -499,7 +499,7 @@ int dbd_db_ping (SV * dbh)
     }
 
     /* No matter what state we are in, send an empty query to the backend */
-    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.17.0 */");
+    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.18.0 */");
     status = PQresultStatus(result);
     PQclear(result);
     if (PGRES_FATAL_ERROR == status) {
--- lib/Bundle/DBD/Pg.pm
+++ lib/Bundle/DBD/Pg.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use 5.008001;
 
-our $VERSION = '3.17.0';
+our $VERSION = '3.18.0';
 
 1;
 
