6119bb09ee0ab50417b376bc653cb3a2302abfc2
Rainer Weikusat <rweikusat@talktalk.net>
2025-05-06 19:13:49 +0100

    documented async connect

--- Pg.pm
+++ Pg.pm
@@ -4310,6 +4310,16 @@ as you don't need it anymore.
     $count = $sth2->fetchall_arrayref()->[0][0];
   }
 
+=head3 Asynchronous Connect
+
+Passing the attribute pg_async_connect to the DBI connect method, eg,
+
+  $dbh = DBI->connect('dbi:Pg:...', $username, $password,
+                      { pg_async_connect => 1 });
+
+starts an asynchronous connect. The C<pg_continue_connect> method must
+be used afterwards to complete the connection establisment process.
+
 =head2 Array support
 
 DBD::Pg allows arrays (as arrayrefs) to be passed in to both 
