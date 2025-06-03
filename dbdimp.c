9608390e74103d25993efb0c11b5c7fdd6d07c01
Rainer Weikusat <rweikusat@talktalk.net>
2025-06-03 18:00:08 +0100

    use correct TRACE macros for PQcancel call in pg_db_cancel  dbd_st_cancel

--- dbdimp.c
+++ dbdimp.c
@@ -5455,7 +5455,7 @@ int pg_db_cancel(SV *h, imp_dbh_t *imp_dbh)
     cancel = PQgetCancel(imp_dbh->conn);
 
     /* This almost always works. If not, free our structure and complain loudly */
-    TRACE_PQGETCANCEL;
+    TRACE_PQCANCEL;
     if (! PQcancel(cancel,errbuf,sizeof(errbuf))) {
         TRACE_PQFREECANCEL;
         PQfreeCancel(cancel);
@@ -5640,7 +5640,7 @@ int dbd_st_cancel(SV *sth, imp_sth_t *imp_sth)
     cancel = PQgetCancel(imp_dbh->conn);
 
     /* This almost always works. If not, free our structure and complain loudly */
-    TRACE_PQGETCANCEL;
+    TRACE_PQCANCEL;
     if (!PQcancel(cancel, errbuf, sizeof(errbuf))) {
         TRACE_PQFREECANCEL;
         PQfreeCancel(cancel);
