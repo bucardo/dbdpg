cce39658682fa67a91b1d6faeb806df4e62b4e71
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-17 12:12:52 +0100

    set imp_dbh->async_sth when a prepare was successfully sent from pg_st_prepare_statement

--- dbdimp.c
+++ dbdimp.c
@@ -2552,9 +2552,10 @@ static int pg_st_prepare_statement (pTHX_ SV * sth, imp_sth_t * imp_sth)
         TRACE_PQSENDPREPARE;
         status = PQsendPrepare(imp_dbh->conn, imp_sth->prepare_name, statement, params,
                                imp_sth->PQoids);
-        if (status)
+        if (status) {
             imp_sth->async_status = STH_ASYNC_PREPARE;
-        else {
+            imp_dbh->async_sth = imp_sth;
+        } else {
             status = PGRES_FATAL_ERROR;
             _fatal_sqlstate(aTHX_ imp_dbh);
         }
