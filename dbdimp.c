e13ce8f7a9bd3e6bef47c48d7776583688f5eaaa
Greg Sabino Mullane <greg@turnstep.com>
2026-05-22 12:12:34 -0400

    Bump version to 3.21.0 [ci skip]

--- dbdimp.c
+++ dbdimp.c
@@ -641,7 +641,7 @@ int dbd_db_ping (SV * dbh)
 
     /* No matter what state we are in, send an empty query to the backend */
     TRACE_PQEXEC;
-    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.20.3 */");
+    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.21.0 */");
     TRACE_PQRESULTSTATUS;
     status = PQresultStatus(result);
     TRACE_PQCLEAR;
