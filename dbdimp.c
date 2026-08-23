a91f787d60494a9edf27d375af3f50316eb70430
Greg Sabino Mullane <greg@turnstep.com>
2026-08-23 09:42:19 -0400

    Version 3.21.1, released August 23, 2026

--- dbdimp.c
+++ dbdimp.c
@@ -768,7 +768,7 @@ int dbd_db_ping (SV * dbh)
 
     /* No matter what state we are in, send an empty query to the backend */
     TRACE_PQEXEC;
-    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.21.0 */");
+    result = PQexec(imp_dbh->conn, "/* DBD::Pg ping test v3.21.1 */");
     TRACE_PQRESULTSTATUS;
     status = PQresultStatus(result);
     TRACE_PQCLEAR;
